b43b31fe4a86cf7b69abb28853936297875e776b
Fabio Soares <fabiosoares@outlook.com.br>
2020-10-07 11:22:03 -0300

    Add feature to read csv/txt file and input data

--- change_coordinates.rb
+++ change_coordinates.rb
@@ -5,4 +5,8 @@ class ChangeCoordinates
     @data = params
     @file = File.open(params).read if params.match(/csv|txt/)
   end
+
+  def read_data(data_or_file)
+    @read_data = data_or_file.gsub(/\n/, ',').split(',').reject { |d| d.empty? }
+  end
 end
